e32d350fea7d007456fb27adc0d4f3e68e583128
javaidb <javaidbaksh@gmail.com>
2024-11-11 02:00:34 -0500

    Format changes

--- models/soc_estimation.py
+++ models/soc_estimation.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-def coulomb_counting(current, time, initial_soc, capacity):
+def coulomb_counting(current, time, initial_soc_percent, capacity):
+    initial_soc = initial_soc_percent/100
     dt = np.diff(time)
     charge = np.cumsum(current[1:] * dt)
     soc = initial_soc - charge / (capacity * 3600)
-    return np.clip(soc, 0, 1)
+    return 100*np.clip(soc, 0, 1)
--- utils/data_generator.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def generate_battery_data(num_samples):
-    time = np.linspace(0, 3600, num_samples)
-    voltage = 3.7 + 0.3 * np.sin(time / 1000)
-    current = 2 + 0.5 * np.random.randn(num_samples)
-    temperature = 25 + 5 * np.random.randn(num_samples)
-    
-    return time, voltage, current, temperature
